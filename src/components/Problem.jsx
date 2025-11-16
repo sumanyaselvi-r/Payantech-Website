9621cecc56f3bf7f7139b44e95adb151958a3798
sumanyaselvi-r <145419732+sumanyaselvi-r@users.noreply.github.com>
2025-11-16 21:33:05 +0530

    Update Problem.jsx

--- src/components/Problem.jsx
+++ src/components/Problem.jsx
@@ -28,11 +28,14 @@ const ProblemSection = () => {
       >
         <h2 className="problem-title">Tech Holding You Back</h2>
         <p className="problem-subtitle">
-          Every business starts with a dream to grow, reach more people, and make a difference.
-          <br></br>
-          But in today’s fast moving digital world, technology can get complicated.
+      
+ Every business starts with a dream to grow, reach more people,and make a difference.
+ <br></br>
+But in today’s fast-moving digital world, technology can get complicated.
 <br></br><br></br>
-That’s where <span>Payantech Solution</span> comes in. We make technology simple, clear, and fast, so your business can run smoothly and grow every day.
+That’s where <span>Payantech Solution</span> comes in.
+ <br></br>
+We make technology simple and fast, so your business can run smoothly and grow.
         </p>
       </motion.div>
     </section>
